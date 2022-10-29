24fe02f8335c118eddafe6c46d35b42f02fd7e5f
OgechukwuCSC205 <yomacorp54@gmail.com>
2022-10-29 21:24:56 +0100

    Website version done, correct commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import profile from "./image/paint.jpg";
+import share from "./image/shareicon2.jpg";
+import slack from "./image/slack.jpg";
+import github from "./image/github.jpg";
+import inforgood from "./image/I4G.jpg"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <img src={share} alt="share" className="share-icon" />
+      
+      <div className="profile">
+        <img src={profile} alt="Yoma Ata" width="100px" height="auto" />
+      </div>
+      <p className="profile-name">Ata Ogechukwu Marilyn</p>
+      <a href="https://training.zuri.team/" id="" className="button-link">
+        Twitter Link
+      </a>
+      <a href="https://training.zuri.team/" id="" className="button-link">
+        Zuri Team
+      </a>
+      <a href="https://training.zuri.team/" id="" className="button-link">
+        Zuri Books
+      </a>
+      <a href="https://training.zuri.team/" id="" className="button-link">
+        Python Books
+      </a>
+      <a href="https://training.zuri.team/" id="" className="button-link">
+        Background checks for Coders
+      </a>
+      <a href="https://training.zuri.team/" id="" className="button-link">
+        Design Books
+      </a>
+
+     
+      <img src={slack} alt="share" className="footer-icon" />
+      <img src={github} alt="share" className="footer-icon git-icon" />
+      
+
+      <footer>
+        <hr />
+        <div className="footer-text">
+          <p className="zuri-text">Zuri<span>.</span>Internship</p>
+          <p className="hng-text">HNG Internship 9 Frontend Task</p>
+          <img src={inforgood} alt="share" className="good-img" />
+        </div>
+      </footer>
     </div>
   );
 }
